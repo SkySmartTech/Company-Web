89fcdd7b97325e04d9be2fc143af8c1048f47354
SewHewage <tsewmini.hewage@gmail.com>
2025-06-16 08:23:48 +0530

    indes.html clients logos

--- Homepage/clients.js
+++ Homepage/clients.js
@@ -1,8 +1,20 @@
   const logos = [
-    "Homepage/img/clients_logo/client_logo4.png",
-    "Homepage/img/clients_logo/client_logo3.png",
     "Homepage/img/clients_logo/client_logo1.png",
-    "Homepage/img/clients_logo/client_logo2.png"
+    "Homepage/img/clients_logo/client_logo2.png",
+    "Homepage/img/clients_logo/client_logo3.png",
+    "Homepage/img/clients_logo/client_logo4.png",
+    "Homepage/img/clients_logo/client_logo5.png",
+    "Homepage/img/clients_logo/client_logo6.png",
+    "Homepage/img/clients_logo/client_logo7.png",
+    "Homepage/img/clients_logo/client_logo8.png",
+    "Homepage/img/clients_logo/client_logo9.png",
+    "Homepage/img/clients_logo/client_logo10.png",
+    "Homepage/img/clients_logo/client_logo11.png",
+    "Homepage/img/clients_logo/client_logo12.png",
+    "Homepage/img/clients_logo/client_logo13.png",
+    "Homepage/img/clients_logo/client_logo14.png",
+    "Homepage/img/clients_logo/client_logo15.png",
+    "Homepage/img/clients_logo/client_logo16.png"
   ];
 
   let current = 0;
